b284794e8d74d29cf9e084f4c05fe4b32a8731ee
Niilo <niilo.raisanen@gmail.com>
2025-05-16 11:13:59 +0300

    first

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const { Pool } = require('pg');
+const bcrypt = require('bcrypt');
+const path = require('path');
+
+const pool = new Pool({
+  connectionString: 'your-railway-database-url'
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+
+  await pool.query(
+    'INSERT INTO users (username, email, password_hash) VALUES ($1, $2, $3)',
+    [username, email, hash]
+  );
+  res.send('Registered!');
+});
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
